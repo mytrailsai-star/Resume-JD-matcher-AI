abbffaf050bcfbe7bdc1bc8fb8ee649c809b0d59
Divya-banoth <banothdivya740@gmail.com>
2026-03-21 14:04:31 +0530

    Apply gitignore properly

--- start_project.ps1
+++
@@ -1,9 +0,0 @@
-cd C:\Users\Jaija\Desktop\python_ai\ai_projects\resume_jd_matcher
-
-# Activate venv
-.\venv\Scripts\Activate.ps1
-
-# Start Streamlit
-streamlit run streamlit_app.py
- Local URL: http://localhost:8502
-  Network URL: http://10.160.143.66:8502
